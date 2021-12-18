82e592c290c2ed921809326f894291dbeb4c5297
Silvia <svgonzalezramos@gmail.com>
2021-12-18 11:34:05 -0300

    cambios es front

--- sallqa_pacha/sallqa_pacha/settings/base.py
+++ sallqa_pacha/sallqa_pacha/settings/base.py
@@ -48,7 +48,10 @@ INSTALLED_APPS = [
     'apps.iniciarsesion',
     'apps.objetivosonu',
     'apps.registrar',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
