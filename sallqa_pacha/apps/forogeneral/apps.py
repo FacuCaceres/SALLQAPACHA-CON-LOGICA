cac687267e85662d0eb02dacbfc3fed34992a1c8
Silvia <svgonzalezramos@gmail.com>
2021-12-19 00:11:12 -0300

    Reformas Front

--- sallqa_pacha/apps/forogeneral/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ForogeneralConfig(AppConfig):
-    name = 'forogeneral'
--- sallqa_pacha/apps/forogeneral/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def Foro_General(request):
-    return render (request,'forogeneral/FOROGENERAL.html' )
